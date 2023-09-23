5b7e8966db3f819bef9549f93f1117bfdd946200
Murilo Vieira <murilo12super@gmail.com>
2023-09-23 16:12:32 -0300

    Inicio do projeto

---
+++ src/main/java/com/muriloCruz/ItGames/ItGamesApplication.java
@@ -0,0 +1,13 @@
+package com.muriloCruz.ItGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muriloCruz/ItGames/ItGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muriloCruz.ItGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
